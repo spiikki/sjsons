ce9e8606efb504376747099bb50bac7f2e1bba8c
spiikki sillanpää <spiikki@nalleperhe.net>
2017-11-01 21:21:29 +0200

    basic data loading

--- src/server.js
+++ src/server.js
@@ -2,21 +2,46 @@
 var express = require('express');
 var server = express();
 
+// loading fileIO
+var fs = require('fs');
+
 // load database
+var database = {};
+fs.readFile('/data/items.json', (err, data) => {
+	if (err) throw err;
+	database = JSON.parse(data);
+});
+
+// follow filter-list and load on change
+var hardFilter = {};
+fs.readFile('/data/hiddenItems.json', (err, data) => {
+	if (err) throw err;
+	hardFilter = JSON.parse(data);
+});
+
+fs.watchFile('/data/hiddenItems.json', {interval: 500}, (curr, prev) => {
+	console.log(`${curr.mtime} : hiddenItems changed! (maybe)`);
+	fs.readFile('/data/hiddenItems.json', (err, data) => {
+		if (err) throw err;
+		hardFilter = JSON.parse(data);
+	});
+
+});
 
 // handle API-calls
 var apiHandler = express.Router();
-apiHandler.use(function(req, res, next) {
+apiHandler.use((req, res, next) => {
 	console.log('TODO: validate API-call');
 	next();	
 });
 
-apiHandler.get('/', function(req, res, next) {
+apiHandler.get('/', (req, res, next) => {
 	console.log('api root called');
-	res.jsonp({this : 'dog'});
+	res.jsonp({this : 'dog', hardFilter: hardFilter});
+
 });
 
-apiHandler.all('*', function(req, res) {
+apiHandler.all('*', (req, res) => {
 	console.log('un-assigned call');
 	res.status(500).jsonp({error: 'don\'t go there'});
 });
@@ -25,3 +50,6 @@ server.use('/api', apiHandler);
 
 // start server
 server.listen(8080);
+
+
+

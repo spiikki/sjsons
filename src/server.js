e88b236cd7374902c85644746b8043deb7d83aa7
spiikki sillanpää <spiikki@nalleperhe.net>
2017-11-02 19:58:50 +0200

    working query filtering and cleaned up POST/DELETE methods

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 // loading lodash
 var _ = require('lodash');
+// laoding body-parser
 var bodyParser = require('body-parser');
 
 // loading express
 var express = require('express');
 var server = express();
 
+// dig JSON data from POST/DELETE bodies
 server.use(bodyParser.urlencoded( {
 	extended: true
 }));
-
-express.json();
 
 // loading fileIO
 var fs = require('fs');
@@ -45,30 +45,41 @@ fs.watchFile('/data/hiddenItems.json', {interval: 500}, (curr, prev) => {
 
 // handle API-calls  -------------------------------------------------------
 var apiHandler = express.Router();
-apiHandler.use((req, res, next) => {
-	console.log('TODO: validate API-call');
-	next();	
-});
 
 // let's handle queries
 apiHandler.get('/', (req, res, next) => {
-	console.log('api root called');
-	res.jsonp(_.filter(database, req.query));
+	var result = database;
+
+	_.each(hardFilter, (value, key) => {
+		result = _.reject(result, { type: value } );
+	});
+
+	res.jsonp(_.filter(result, req.query));
 });
 
 // create new item!
 apiHandler.post('/', (req, res, next) => {
+	// work as logger, save all posts
 	console.log("inserting data:");
 	console.log(req.body);
-	database.push(req.body)
-	res.jsonp({idea: "i might have.."});
+	database.push(req.body);
+	res.status(202).jsonp(req.body); // 202 Accepted 
+
+	//TODO: save it!!
 });
 
 // delete item x(
 apiHandler.delete('/', (req, res, next) => {
 	console.log("deleting data: ");
 	console.log(req.body);
-	res.jsonp({exterminate: "exterminate"});
+
+	if( _.find(database, req.body) == undefined) {
+		res.status(404).jsonp({error: "not in the library"});
+	} else {
+		database.pop(req.body);
+		res.status(202).jsonp(req.body);
+	}
+	//TODO: save it!!
 });
 
 // atleast trying to respond politely
@@ -78,7 +89,6 @@ apiHandler.all('*', (req, res) => {
 });
 
 server.use('/api', apiHandler);
-
 
 // start server  -----------------------------------------------------------
 server.listen(8080);

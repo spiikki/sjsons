1246a22fdfe7b983ce3537b344d9c8fa7ee1c29c
spiikki sillanpää <spiikki@nalleperhe.net>
2018-02-23 20:22:25 +0200

    copypaste transfer from dev

--- src/server.js
+++ src/server.js
@@ -4,21 +4,22 @@ const _ = require('lodash');
 const bodyParser = require('body-parser');
 // loading async
 const async = require('async');
+// loading mongodb 
+const db = require('mongodb').MongoClient;
+const mongo = require('mongodb');  
 
 // loading express
 const express = require('express');
 const server = express();
 
 // dig JSON data from POST/DELETE bodies
-server.use(bodyParser.urlencoded( {
+server.use(bodyParser.json( {
 	extended: true
 }));
 
 // announcing the stars
-let database = {};
-let hardFilter = {};
-let loadingDB = true;
-let loadingHardFilter = true;
+let database = "mydb";
+let url = "mongodb://localhost:27017";
 
 // handle API-calls  -------------------------------------------------------
 const apiHandler = express.Router();
@@ -28,37 +29,72 @@ apiHandler.use((req,res,next) => {
 	next();
 });
 
-// let's handle queries 
-apiHandler.get('/', (req, res, next) => {
-	// make query to database and reply result
-	let result = {};
-	return res.jsonp(_.filter(result, req.query));
+// let's handle queries
+apiHandler.get('/', (req, res, next) => { 
+        // make query to database and reply result                                                
+        console.log("get all");                                                                   
+        db.connect(url, (err, client) => {
+                if(err) res.status(500).jsonp(err);
+                const kanta = client.db(database);
+                const dokki = kanta.collection('wizard');
+                dokki.find({}).toArray((err, entries) => {
+                        if(err) res.status(500).jsonp(err);
+                        res.status(200).jsonp(entries);
+                });
+                client.close();
+        });
 });
+
+apiHandler.get('/user/:uid', (req, res, next) => {
+        // make query to database and reply result
+        console.log("get byUserId " + req.params.uid);
+        db.connect(url, (err, client) => {
+                if(err) res.status(500).jsonp(err);
+                const kanta = client.db(database);
+                const dokki = kanta.collection('wizard');
+                dokki.find({userId : req.params.uid}).toArray((err, entries) => {
+                        if(err) res.status(500).jsonp(err);
+                        res.status(200).jsonp(entries);
+                });
+                client.close();
+        });
+});
+
+apiHandler.get('/session/:id', (req, res, next) => {
+        // make query to database and reply result
+        console.log("get bySessionId " + req.params.id);
+        db.connect(url, (err, client) => {
+                if(err) res.status(500).jsonp(err);
+                const kanta = client.db(database);
+                const dokki = kanta.collection('wizard');
+                let oid = new mongo.ObjectId(req.params.id);
+                dokki.find({_id : oid}).toArray((err, entries) => {
+                        if(err) res.status(500).jsonp(err);
+                        res.status(200).jsonp(entries);
+                });
+                client.close();
+        });
+});
+
 
 // create new item!
 apiHandler.post('/', (req, res, next) => {
-	// work as logger, save all posts
-	console.log('inserting data:');
-	console.log(req.body);
-	// insert document to database
+        // work as logger, save all posts
+        console.log('inserting data:');
+        console.log(req.body);
+        // insert document to database
+        db.connect(url, (err, client) => {
+                if(err) res.status(500).jsonp(err);
+                const kanta = client.db(database);
+                const dokki = kanta.collection('wizard');
+                let id = {};
 
-	//
-
-	res.status(202).jsonp(req.body); // 202 Accepted 
-	return true;
-});
-
-// delete item x(
-apiHandler.delete('/', (req, res, next) => {
-	if( !true ) { // document not found from database
-		return res.status(404).jsonp({error: 'not in the library'});
-	} else {
-		console.log('deleting data: ');
-		console.log(req.body);
-		// delete document
-		res.status(202).jsonp(req.body);
-		return true;
-	}
+                id = dokki.insertOne(req.body, (err, entry, test) => {
+                        if(err) res.status(500).jsonp(err);
+                        client.close();
+                        return res.status(200).jsonp({ id : entry.insertedId });
+                });
+        });
 });
 
 // atleast trying to respond politely
